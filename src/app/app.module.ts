08d258226b7b4a563a26596c5e5f46624cd2ed33
VictorSouza_ <victorsouza0106@gmail.com>
2022-03-30 02:16:15 -0300

    Adding license page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { LoginComponent } from './pages/login/login.component';
+import { LicenseComponent } from './pages/license/license.component';
 
 // MATERIAL IMPORTS
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LicenseComponent
   ],
   imports: [
     BrowserModule,
