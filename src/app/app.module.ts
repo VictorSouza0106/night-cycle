5ee2a9f8c11f4c09d5a156fd630f2bfebc8a8099
VictorSouza_ <victorsouza0106@gmail.com>
2022-05-18 19:29:38 -0300

    Fix - Error import - file not exists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { LoginComponent } from './pages/login/login.component';
 import { LicenseComponent } from './pages/license/license.component';
-import { CoursesComponent } from './pages/courses/courses.component';
 
 // MATERIAL IMPORTS
 
@@ -17,7 +16,6 @@ import { CoursesComponent } from './pages/courses/courses.component';
     AppComponent,
     LoginComponent,
     LicenseComponent,
-    CoursesComponent
   ],
   imports: [
     BrowserModule,
